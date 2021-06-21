5e9a0a4ae24bff739674aa0a71ac1176c5f4e0eb
De Aunte Hall <deauntehall@Des-MacBook-Pro.local>
2021-06-21 15:30:10 -0700

    add if else statements to js

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,18 @@ if (Avenger === "Iron-Man") {
 } else if (Avenger === "Black-widow") {
   $('#GO').show();
 }
+
+if (JusticeLeague === "Super-Man") {
+  $('#JavaScript').show();
+} else if (Avenger === "Wonder-Woman") {
+  $('#Csharp').show();
+} else if (Avenger === "The-Flash") {
+  $('#Python').show();
+} else if (Avenger === "Bat-Man") {
+  $('#Ruby').show();
+} else if (Avenger === "Green-Lantern") {
+  $('#GO').show();
+}
   
 
   
