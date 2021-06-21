9082c99f709d6805ee0c2734863185483cf10f9c
De Aunte Hall <deauntehall@Des-MacBook-Pro.local>
2021-06-21 15:11:46 -0700

    add if else to js and id's to css

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
- $(document).ready(function(){
-  function submitButton()  {
-   // event.preventDefault(); 
+$(document).ready(function(){
+    function submitButton()  {
+    event.preventDefault(); 
     const Avenger = $("input[name=Avenger]:checked").val();
     const JusticeLeague = $("input[name=Justice-League]:checked").val();
     const Xmen = $("input[name=Xmen]:checked").val();
     const SuperVilliain = $("input[name=destination]:checked").val();
-    const exercise = $("input[name=Exercise]:checked").val();
-  alert(Avenger+JusticeLeague+Xmen+SuperVilliain+DragonBall);
+    const DragonBall = $("input[name=Exercise]:checked").val();
   }
-});
+}); 
+  alert(Avenger+JusticeLeague+Xmen+SuperVilliain+DragonBall);
 
+
+if (Avenger === "Iron-Man") {
+  $('#JavaScript').show();
+} else if (Avenger === "Hulk") {
+  $('#Csharp').show();
+} else if (Avenger === "Captain-America") {
+  $('#Python').show();
+} else if (Avenger === "Black-Panther") {
+  $('#Ruby').show();
+} else if (Avenger === "Black-widow") {
+  $('#GO').show();
+}
+  
+
+  
+ 
