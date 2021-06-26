aa336bfb85e2c4cd152f3009b6dba7d6df37a008
De Aunte Hall <deauntehall@Des-MacBook-Pro.local>
2021-06-26 10:37:36 -0700

    update html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
-    function submitButton()  {
+    $("form#list").submit(function(event) {
     event.preventDefault(); 
-    const Avenger = $("input[name=Avenger]:checked").val();
+    const Avenger = parseInt($("input[name=Avenger]:checked").val());
     const JusticeLeague = $("input[name=Justice-League]:checked").val();
     const Xmen = $("input[name=Xmen]:checked").val();
     const SuperVilliain = $("input[name=destination]:checked").val();
     const DragonBall = $("input[name=Exercise]:checked").val();
-  }
+    const result =
+  });
 }); 
   alert(Avenger+JusticeLeague+Xmen+SuperVilliain+DragonBall);
 
 
-if (Avenger === "Iron-Man") {
+
   $('#JavaScript').show();
-} else if (Avenger === "Hulk") {
+
   $('#Csharp').show();
-} else if (Avenger === "Captain-America") {
+
   $('#Python').show();
-} else if (Avenger === "Black-Panther") {
+
   $('#ruby').show();
-} else if (Avenger === "Black-widow") {
+
   $('#GO').show();
-}
+
 
 if (JusticeLeague === "Super-Man") {
   $('#JavaScript').show();
